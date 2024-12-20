be54782711347159708d0c326770732e128143e8
deanmchris <deanmchris@gmail.com>
2024-12-20 11:11:41 -0600

    Update time mangagement to be smarter
    Now, instead of taking a fixed amount of time for every move, the engine
    transitions from taking less time at the beginning of the game, more time
    in the mid-game, and a more linear amount of time towards the end game.

--- engine/timer.go
+++ engine/timer.go
@@ -1,8 +1,6 @@
 package engine
 
-import (
-	"time"
-)
+import "time"
 
 const (
 	MovesToGoTimingFormat = iota
@@ -10,31 +8,48 @@ const (
 	InfiniteTimeFormat
 	NoFormat
 
-	TimeBuffer int64 = 150
+	TimeBuffer          int64 = 100
+	AvgExpectedNumMoves int64 = 70
+	SmallestDivide      int64 = 8
 )
 
 type Timer struct {
-	startTime    time.Time
-	searchTime   int64
-	Stopped      bool
-	infiniteTime bool
+	startTime       time.Time
+	searchTime,
+	movesToGo,
+	movesToGoHalved,
+	coeff           int64
+	Stopped,
+	infiniteTime    bool
 }
 
-func (timer *Timer) CalculateSearchTime(timeFormat int, movesToGo, timeLeft, timeInc int64) {
+func (timer *Timer) SetInitValues(timeFormat int, movesToGo int64) {
+	if timeFormat != MovesToGoTimingFormat {
+		movesToGo = AvgExpectedNumMoves
+	}
+	timer.movesToGo = movesToGo
+	timer.movesToGoHalved = movesToGo / 2
+	timer.coeff = (timer.movesToGoHalved * timer.movesToGoHalved) / 50
+}
+
+func (timer *Timer) CalculateSearchTime(timeFormat int, timeLeft, timeInc int64, numOfMoves uint16) {
 	timer.Stopped = false
+	bonus := timeInc / 2
+	divide := timer.CalcTimeLeftDivide(numOfMoves)
+
 	switch timeFormat {
-	case MovesToGoTimingFormat:
-		timer.searchTime = timeLeft / movesToGo + (timeInc / 2)
-		timer.infiniteTime = false
-	case SuddenDeathTimeFormat:
-		timer.searchTime = timeLeft / 5 + (timeInc / 2)
+	case MovesToGoTimingFormat, SuddenDeathTimeFormat:
+		timer.searchTime = timeLeft / divide + bonus
 		timer.infiniteTime = false
 	case InfiniteTimeFormat:
 		timer.infiniteTime = true
 	}
 
 	if timer.searchTime > TimeBuffer {
-		timer.searchTime -= TimeBuffer
+		timer.searchTime -= bonus
+		if timer.searchTime > TimeBuffer {
+			timer.searchTime -= TimeBuffer
+		}
 	}
 }
 
@@ -46,4 +61,14 @@ func (timer *Timer) Update()  {
 	if !timer.infiniteTime && time.Since(timer.startTime).Milliseconds() >= timer.searchTime {
 		timer.Stopped = true
 	}
+}
+
+func (timer *Timer) CalcTimeLeftDivide(numOfMoves uint16) int64 {
+	numOfMovesInt64 := int64(numOfMoves)
+	
+	if numOfMovesInt64 <= timer.movesToGoHalved {
+		return ((numOfMovesInt64 - timer.movesToGoHalved) * 
+				(numOfMovesInt64 - timer.movesToGoHalved)) / timer.coeff + SmallestDivide
+	}
+	return (2 * (numOfMovesInt64 - timer.movesToGoHalved)) / timer.coeff + SmallestDivide
 }
